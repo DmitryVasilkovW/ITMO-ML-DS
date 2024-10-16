d8ec02921431ba385bd422d10c9cfc4ce3378e8e
Dmitry Vasilkov <113594620+DmitryVasilkovW@users.noreply.github.com>
2024-10-16 16:44:21 +0300

    fix: rate

--- ebay_parser/main.py
+++ ebay_parser/main.py
@@ -4,13 +4,13 @@ from ebay_parser.data_saver import DataSaver
 
 
 def main():
-    #url = "https://www.ebay.com/b/Electronics/bn_7000259124"
-    url = "https://www.ebay.com/p/11052276253?iid=375567675873&var=644492689896"
+    # url = "https://www.ebay.com/b/Cell-Phones-Smartphones/9355/bn_320094"
+    url = "https://www.ebay.com/b/Cell-Phones-Smartphones/9355/bn_320094?rt=nc&_sop=10"
     parser = DataParser(url)
 
     html = parser.fetch_page()
 
-    items = parser.parse_items(html)
+    items = parser.parse_page()
 
     # #cleaned_items = [DataCleaner.clean_data(item) for item in items]
     #
@@ -20,6 +20,7 @@ def main():
     # DataSaver.save_to_arff(cleaned_items, "datasets/processed_data/data.arff")
 
     print(items)
+
 
 if __name__ == "__main__":
     main()
