92e9bb6155b2b763751f1d978eced920c05235da
Dmitry Vasilkov <113594620+DmitryVasilkovW@users.noreply.github.com>
2024-10-18 04:54:18 +0300

    feat: normalizing

--- ebay_parser/utils/normalize_utils.py
+++ ebay_parser/utils/normalize_utils.py
@@ -1,33 +1,36 @@
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder, MinMaxScaler
 
-df = pd.read_csv('../../data/ebay_smartphones_data.tsv', sep='\t')
 
-numeric_cols = ['price RUB', 'rating', 'reviews', 'Screen Size in', 'Storage Capacity GB', 'RAM GB',
-                'Camera Resolution MP']
-numeric_cols = [col for col in numeric_cols if col in df.columns]
+def normalize_df():
+    df = pd.read_csv('../../data/ebay_smartphones_data.tsv', sep='\t')
 
-for col in numeric_cols:
-    df[col] = pd.to_numeric(df[col], errors='coerce')
+    numeric_cols = ['rating', 'reviews', 'Screen Size in', 'Storage Capacity GB', 'RAM GB',
+                    'Camera Resolution MP']
+    numeric_cols = [col for col in numeric_cols if col in df.columns]
 
-if numeric_cols:
-    df[numeric_cols] = df[numeric_cols].apply(lambda x: x.fillna(x.mean()))
+    for col in numeric_cols:
+        df[col] = pd.to_numeric(df[col], errors='coerce')
 
-categorical_cols = ['product condition', 'Features', 'Brand', 'Model', 'Processor', 'Chipset Model', 'Colour',
-                    'Lock Status', 'Network', 'Operating System', 'Connectivity']
-categorical_cols = [col for col in categorical_cols if col in df.columns]
+    if numeric_cols:
+        df[numeric_cols] = df[numeric_cols].fillna(df[numeric_cols].mean())
 
-if categorical_cols:
-    df[categorical_cols] = df[categorical_cols].fillna('Unknown')
+    categorical_cols = ['title', 'product condition', 'Features', 'Brand', 'Model', 'Processor', 'Chipset Model',
+                        'Colour',
+                        'Lock Status', 'Network', 'Operating System', 'Connectivity']
+    categorical_cols = [col for col in categorical_cols if col in df.columns]
 
-label_encoders = {}
-for col in categorical_cols:
-    le = LabelEncoder()
-    df[col] = le.fit_transform(df[col])
-    label_encoders[col] = le
+    if categorical_cols:
+        df[categorical_cols] = df[categorical_cols].fillna('Unknown')
 
-if numeric_cols:
-    scaler = MinMaxScaler()
-    df[numeric_cols] = scaler.fit_transform(df[numeric_cols])
+    label_encoders = {}
+    for col in categorical_cols:
+        le = LabelEncoder()
+        df[col] = le.fit_transform(df[col])
+        label_encoders[col] = le
 
-df.to_csv('../../data/ebay_smartphones_data.csv', index=False)
+    if numeric_cols:
+        scaler = MinMaxScaler()
+        df[numeric_cols] = scaler.fit_transform(df[numeric_cols])
+
+    df.to_csv('../../data/ebay_smartphones_data.csv', index=False)
